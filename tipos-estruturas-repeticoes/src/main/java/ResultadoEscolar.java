da8c7cdff2d61e3025c06fda6df8e386a2336212
Edilberto Morais <edilbertocmorais@gmail.com>
2023-08-30 17:53:36 -0300

    add comentários a Classe ResultadoEscolar.

--- tipos-estruturas-repeticoes/src/main/java/ResultadoEscolar.java
+++ tipos-estruturas-repeticoes/src/main/java/ResultadoEscolar.java
@@ -2,9 +2,12 @@ public class ResultadoEscolar {
     public static void main(String[] args) {
         int nota = 8;
 
+        // Analisa se a nota é maior ou igual a 7.
         if (nota >= 7){
+            // Caso seja verdadeiro, será impresso a mensagem abaixo.
             System.out.println("Aprovado.");
         }else {
+            // Caso seja falso, será impresso a mensagem abaixo.
             System.out.println("Reprovado.");
         }
     }
