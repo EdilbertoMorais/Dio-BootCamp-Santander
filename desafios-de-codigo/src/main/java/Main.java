fcf58fa76e3867f4a6e245e6463ee3ef1e6e05a5
Edilberto Morais <edilbertocmorais@gmail.com>
2023-09-06 19:12:02 -0300

    add a classe Main como resolução do desafio da DIO no Bootcamp Santander.

---
+++ desafios-de-codigo/src/main/java/Main.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int saldoTotal = obterValorValido(scanner);
+        int valorSaque = obterValorValido(scanner);
+
+        calcularSaldoAtualizado(saldoTotal, valorSaque);
+    }
+
+    private static int obterValorValido(Scanner scanner) {
+        int valor;
+        do {
+            valor = scanner.nextInt();
+            if (valor < 0)
+                System.out.println("Valor não pode ser menor que 0");
+        } while (valor < 0);
+        return valor;
+    }
+
+    private static void calcularSaldoAtualizado(int saldoTotal, int valorSaque) {
+        if (saldoTotal > valorSaque) {
+            saldoTotal = saldoTotal - valorSaque;
+            System.out.print("Saque realizado com sucesso. Novo saldo: " + saldoTotal);
+        } else {
+            System.out.println("Saldo insuficiente. Saque nao realizado!");
+        }
+    }
+}
