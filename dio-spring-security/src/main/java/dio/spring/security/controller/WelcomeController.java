348487e568f66dff50ba36815fcb89574f269153
Edilberto Morais <edilbertocmorais@gmail.com>
2023-10-17 17:58:51 -0300

    autenticando o banco de dados

--- dio-spring-security/src/main/java/dio/spring/security/controller/WelcomeController.java
+++ dio-spring-security/src/main/java/dio/spring/security/controller/WelcomeController.java
@@ -1,6 +1,5 @@
-package dio.spring.security;
+package dio.spring.security.controller;
 
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
