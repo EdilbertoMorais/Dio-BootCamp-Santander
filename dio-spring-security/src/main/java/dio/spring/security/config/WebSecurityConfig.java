c7da254090d4401392a6a1a4d53ebd0cdda6560e
Edilberto Morais <edilbertocmorais@gmail.com>
2023-10-17 17:58:51 -0300

    Autenticando o banco de dados

--- dio-spring-security/src/main/java/dio/spring/security/config/WebSecurityConfig.java
+++ dio-spring-security/src/main/java/dio/spring/security/config/WebSecurityConfig.java
@@ -1,5 +1,7 @@
-package dio.spring.security;
+package dio.spring.security.config;
 
+import dio.spring.security.config.SecurityDatabaseService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -7,6 +9,7 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
 //Definimos que está é uma classe de configuração;
 @Configuration
@@ -15,6 +18,13 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 //Exige que os usuários tenham um perfil (role) apropriado usando anotações de segurança;
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private SecurityDatabaseService securityService;
+
+    @Autowired
+    public void globalUserDetails(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(securityService).passwordEncoder(NoOpPasswordEncoder.getInstance());
+    }
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
@@ -28,28 +38,29 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 //Somente logins com perfil users ou managers poderão acessar este recurso;
                 .antMatchers("/users").hasAnyRole("USERS","MANAGERS")
                 //Determinamos a forma de solicitar a requisição como Form Login.
-                .anyRequest().authenticated().and().formLogin();
+                .anyRequest().authenticated().and().httpBasic();
     }
-    @Override
-    //Gerenciador de credenciais em memória;
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("user")
-                //Determinam a criptografia aplicada na senha de cada usuário.
-                .password("{noop}user123")
-                /**
-                 * Existem algumas implementações de criptografias utilizadas pelo Spring Security
-                 * Use {bcrypt} for BCryptPasswordEncoder (mais comum);
-                 * Use {noop} for NoOpPasswordEncoder;
-                 * Use {pbkdf2} for Pbkdf2PasswordEncoder;
-                 * Use {scrypt} for SCryptPasswordEncoder;
-                 * Use {sha256} for StandardPasswordEncoder.
-                 */
-                .roles("USERS")
-                .and()
-                .withUser("admin")
-                .password("{noop}master123")
-                .roles("MANAGERS");
-    }
+
+//    @Override
+//    //Gerenciador de credenciais em memória;
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.inMemoryAuthentication()
+//                .withUser("user")
+//                //Determinam a criptografia aplicada na senha de cada usuário.
+//                .password("{noop}user123")
+//                /**
+//                 * Existem algumas implementações de criptografias utilizadas pelo Spring Security
+//                 * Use {bcrypt} for BCryptPasswordEncoder (mais comum);
+//                 * Use {noop} for NoOpPasswordEncoder;
+//                 * Use {pbkdf2} for Pbkdf2PasswordEncoder;
+//                 * Use {scrypt} for SCryptPasswordEncoder;
+//                 * Use {sha256} for StandardPasswordEncoder.
+//                 */
+//                .roles("USERS")
+//                .and()
+//                .withUser("admin")
+//                .password("{noop}master123")
+//                .roles("MANAGERS");
+//    }
 }
 
